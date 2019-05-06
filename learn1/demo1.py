7395ac1bb7ef19edbae4cd5f37eaa41af565ecfc
ZXzxxx <532721245@qq.com>
2019-05-06 14:57:09 +0800

    ZX
    Python Learn1

---
+++ learn1/demo1.py
@@ -0,0 +1,37 @@
+print("hello,world")
+print("你好，世界")
+#name = input("请输入您的名字:")
+#print("hello", name)
+print(3 / 2)
+print(199999999 / 3)
+print(19999999.9 / 3)
+print("helo \' world \" ")
+print(r'\\t\\t\\n')
+print('''hahah
+hehhh
+oooo
+llll
+kkkkk''')
+print(r'''nihao \t\n\t
+hahah
+heihei''')
+
+if 5 > 3:
+    print("5 > 3")
+
+name = None
+print(name)
+
+age = 15
+print(age)
+age = "age=15"
+print(age)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+print(10//3)
+print(10.8//3)
+
